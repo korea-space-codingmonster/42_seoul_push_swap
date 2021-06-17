0be9aa41abf98e7339f44f5db66a8cf2c14bd614
codingmonster <namju890@naver.com>
2021-06-17 13:12:16 +0900

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: napark <napark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 19:23:35 by napark            #+#    #+#             */
-/*   Updated: 2021/06/17 12:58:59 by napark           ###   ########.fr       */
+/*   Updated: 2021/06/17 13:12:03 by napark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int check_ar
 
-int check_error(int argc, char *argv)
+int create_stack(int argc, char **argv)
 {
 //./push_swap "2 5 3 4 2"
 //아무것도 입력 안했을때
@@ -22,18 +22,18 @@ int check_error(int argc, char *argv)
 //하나만 들어있을 때
 //중복
 //숫자(int)외 다른 문자 (음수가능)
-//
-    if (argc < 2)
-        return (0);
+    t_stack *stack;
     
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     int check;
-    t_push_swap     
+    t_push_swap data;
 
-    check = create_stack(argc, argv);
+    if (argc < 2)
+        return (0);
+    data.a = create_stack(argc, argv);
     if (check == 1)
     {
         
